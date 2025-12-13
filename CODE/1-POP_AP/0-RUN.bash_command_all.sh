20acbc32835d185d7cd325f676deda68b26e20e0
Pepper.orz <122876342+yuanlinm@users.noreply.github.com>
2025-12-13 19:05:54 +0800

    Delete CODE directory

--- CODE/1-POP_AP/0-RUN.bash_command_all.sh
+++
@@ -1,9 +0,0 @@
-# python3, now supports the loop in the script: --years xxxx-xxxx
-source .venv/bin/activate && python3 code/1-triple_mapping_pipline.py \
-	--years 2000-2021 \
-	--pollutant-template "data/3.1-CHAP_PM25_avgY_1K_2000_2021_V3/CHAP_PM2.5_Y1K_{year}_V4.nc" \
-	--pollutant-var PM2.5 \
-	--pollutant-name pm25 \
-	--pop-template "data/2-LandScan/landscan-global-{year}-assets/landscan-global-{year}.tif" \
-	--cities "data/1-中国市界行政区划/cities.shp" \
-	--output-dir "output/mapping"
